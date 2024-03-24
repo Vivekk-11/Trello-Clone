3c2c6ef3ed17b833513518ccc78d05e2216b53ba
vivek <chimnanivivek14@gmail.com>
2024-03-24 19:34:23 +0530

    created card modal in the frontend

--- app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/CardItem.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useCardModal } from "@/hooks/use-card-modal";
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 import React from "react";
@@ -8,6 +11,7 @@ interface CardItemProps {
 }
 
 const CardItem = ({ index, card }: CardItemProps) => {
+  const cardModal = useCardModal();
   return (
     <Draggable draggableId={card.id} index={index}>
       {(provided) => (
@@ -16,6 +20,7 @@ const CardItem = ({ index, card }: CardItemProps) => {
           {...provided.draggableProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(card.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {card.title}
--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import ModalProvider from "@/components/providers/ModalProvider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <Toaster />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   );
